4abaa24343932b452c3cecb085045145056e4b8e
Grigoris_Dimitroulakos <dhmhgre@gmail.com>
2023-11-20 22:16:26 +0200

    1) Incorporate code from existing AST base visitor 2) Adapt base visitor for MATHL 3) Create compileunit ast element

---
+++ MATHL/Composite/ASTBaseVisitor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATHL.Composite {
+    public abstract class ASTBaseVisitor<Return, Params> : IASTBaseVisitor<Return, Params> {
+
+        // Visit a specific node and send a variable number of
+        // arguments. The responsibility of the type and sequence
+        // of arguments is on the user. ( box/unboxing for scalars)
+        public virtual Return Visit(IASTVisitableNode node, params Params[] info) {
+            return node.Accept<Return, Params>(this, info);
+        }
+
+        // Visit the children of a specific node and summarize the 
+        // results by the visiting each child 
+        public virtual Return VisitChildren(IASTComposite node, params Params[] info) {
+            Return result = default(Return);
+            Return iResult;
+            foreach (IASTVisitableNode astNode in node) {
+                iResult = astNode.Accept<Return, Params>(this, info);
+                result = Summarize(iResult, result);
+            }
+            return result;
+        }
+
+        public virtual Return Summarize(Return iresult, Return result) {
+            return iresult;
+        }
+    }
+
+    public class MATHLBaseVisitor<Result, Params> : ASTBaseVisitor<Result, Params> {
+
+        public virtual Result VisitContextChildren(ASTComposite node, int context,
+            params Params[] info) {
+            Result result = default(Result);
+            Result iResult;
+            IASTIterator it = node.CreateContextIterator(context);
+            ASTElement astNode;
+            for (it.Init(); it.End() == false; it.Next()) {
+                astNode = it.MCurNode;
+                iResult = astNode.Accept<Result, Params>(this, info);
+                result = Summarize(iResult, result);
+            }
+            return result;
+        }
+
+        public override Result VisitChildren(IASTComposite node, params Params[] info) {
+            ASTComposite n = node as ASTComposite;
+            Result result = default(Result);
+            Result iResult;
+            IASTIterator it = n.CreateIterator();
+            ASTElement astNode;
+            for (it.Init(); it.End() == false; it.Next()) {
+                astNode = it.MCurNode;
+                iResult = astNode.Accept<Result, Params>(this, info);
+                result = Summarize(iResult, result);
+            }
+            return result;
+        }
+
+        public virtual Result VisitCompileUnit(CompileUnit node, params Params[] args) {
+            return VisitChildren(node, args);
+        }
+
+        
+    }
+}
--- MATHL/Composite/ASTConcreteElements.cs
+++ MATHL/Composite/ASTConcreteElements.cs
@@ -5,7 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MATHL.Composite {
-    
 
 
+    public enum NodeType {
+        NT_NA = -1, NT_COMPILEUNIT, NT_ADDITION, NT_SUBTRACTION,
+        NT_MULTIPLICATION, NT_DIVISION, NT_IDENTIFIER, NT_NUMBER,
+        NT_ASSIGNMENT
+    }
+
+    public class CompileUnit : ASTComposite {
+        public const int EXPRESSIONS = 0;
+        public readonly string[] mc_contextNames = { "Expressions" };
+
+        public CompileUnit() :
+            base(1, (int)NodeType.NT_COMPILEUNIT) {
+        }
+
+        public override Return Accept<Return, Params>(IASTBaseVisitor<Return, Params> v,
+            params Params[] info) {
+            MATHLBaseVisitor<Return, Params> visitor = v as MATHLBaseVisitor<Return, Params>;
+            return visitor.VisitCompileUnit(this, info);
+        }
+    }
 }
